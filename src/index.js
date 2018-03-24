c837a23b6dcf6da5d6bcb38243f6298609d9d922
jdotrjs <jdotrjs@example.com>
2018-03-24 15:46:13 -0700

    Checking in the as-written version of the first game

--- src/index.js
+++ src/index.js
@@ -1,16 +1,143 @@
 let game = null
+let platforms = null
+let player = null
+let cursors = null
+let stars = null
+let score = 0
+let scoreText = null
 
 let phaserConfig = {
   type: Phaser.AUTO,
   parent: 'phaser-display',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      // debug: true,
+    },
+  },
   scene: {
-    preload: () => {},
-    create: () => {},
+    preload: preloadFn,
+    create: createFn,
+    update: updateFn,
+    render: renderFn,
   },
 }
 
-window.onload = () => {
-  game = new Phaser.Game(phaserConfig)
+game = new Phaser.Game(phaserConfig)
+
+function preloadFn() {
+  this.load.image('sky',    'assets/sky.png')
+  this.load.image('ground', 'assets/platform.png')
+  this.load.image('star',   'assets/star.png')
+  this.load.image('bomb',   'assets/bomb.png')
+
+  this.load.spritesheet(
+    'dude',
+    'assets/dude.png',
+    { frameWidth: 32, frameHeight: 48 })
 }
+
+function createFn() {
+  this.add.image(400, 300, 'sky')
+  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' })
+
+  platforms = this.physics.add.staticGroup()
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody()
+  platforms.create(600, 400, 'ground')
+  platforms.create(50, 250, 'ground')
+  platforms.create(750, 220, 'ground')
+
+  player = this.physics.add.sprite(100, 450, 'dude')
+  player.setBounce(0.2)
+  player.setCollideWorldBounds(true)
+  this.physics.add.collider(player, platforms)
+
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 },
+  })
+
+  stars.children.iterate(child => {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+  })
+  this.physics.add.collider(stars, platforms)
+  this.physics.add.overlap(player, stars, collectStar, null, this)
+
+  bombs = this.physics.add.group()
+  this.physics.add.collider(bombs, platforms)
+  this.physics.add.collider(player, bombs, hitBomb, null, this)
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  })
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20,
+  })
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  })
+
+  cursors = this.input.keyboard.createCursorKeys()
+}
+
+function collectStar(player, star) {
+  star.disableBody(true, true)
+  score += 10
+  scoreText.setText(`Score: ${score}`)
+
+  console.log(stars.countActive(true))
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate(child => {
+      child.enableBody(true, child.x, 0, true, true)
+      child.setVelocityY(Phaser.Math.Between(0, -200))
+    })
+
+    const x = player.x < 400
+      ? Phaser.Math.Between(400, 800)
+      : Phaser.Math.Between(0, 400)
+
+    const bomb = bombs.create(x, 16, 'bomb')
+    bomb.setBounce(1)
+    bomb.setCollideWorldBounds(true)
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+    bomb.allowGravity = false
+  }
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause()
+  player.setTint(0xff0000)
+  player.anims.play('turn')
+  gameOver = true
+}
+
+function updateFn() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160)
+    player.anims.play('left', true)
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160)
+    player.anims.play('right', true)
+  } else {
+    player.setVelocityX(0)
+    player.anims.play('turn', true)
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.body.setVelocityY(-330)
+  }
+}
+
+function renderFn() {}
